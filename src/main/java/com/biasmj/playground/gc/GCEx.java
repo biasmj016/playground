bb756bb1989e59656e3a8f261821fc58f03fe714
biasmj016 <mjkim031@gmail.com>
2025-01-15 19:30:53 +0900

    가비지 컬렉션 테스트

---
+++ src/main/java/com/biasmj/playground/gc/GCEx.java
@@ -0,0 +1,10 @@
+package com.biasmj.playground.gc;
+
+public class GCEx {
+    public static void main(String[] args) {
+        {
+            byte[] placeholder = new byte[64 * 1024 * 1024];
+        }
+        System.gc();
+    }
+}
