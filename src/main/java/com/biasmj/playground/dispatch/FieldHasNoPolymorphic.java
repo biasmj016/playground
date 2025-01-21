3f05af0822d80590c9c321c5bb069f91d7c68084
biasmj016 <mjkim031@gmail.com>
2025-01-21 14:32:32 +0900

    dispatch method example2

---
+++ src/main/java/com/biasmj/playground/dispatch/FieldHasNoPolymorphic.java
@@ -0,0 +1,35 @@
+package com.biasmj.playground.dispatch;
+
+public class FieldHasNoPolymorphic {
+    static class Father {
+        public int money = 1;
+
+        public Father() {
+            money = 2;
+            showMeTheMoney();
+        }
+
+        public void showMeTheMoney() {
+            System.out.println("I am Father, I have $" + money);
+        }
+    }
+
+    static class Son extends Father {
+        public int money = 3;
+
+        public Son() {
+            money = 4;
+            showMeTheMoney();
+        }
+
+        public void showMeTheMoney() {
+            System.out.println("I am Son, I have $" + money);
+        }
+    }
+
+    public static void main(String[] args) {
+        Father guy = new Son();
+        System.out.println("This guy has $" + guy.money);
+    }
+
+}
